c6aea9f513eaa6a15e0ce2fbf7041c9a408c3a1a
DonnyvanEijk <donnyvaneijk873@gmail.com>
2025-08-07 10:48:51 +0200

    Cleaned up and reworked styling and coloring

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
-import Hero from "@/components/Hero";
+import { Hero } from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <main>
+    <div className="min-h-screen w-full ">
       <Navbar />
-      <Hero />
-    </main>
+      <div className="pt-32 pb-12 flex flex-col items-center justify-center">
+        <Hero />
+      </div>
+    </div>
   );
 }
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,30 @@
-export default function Hero() {
-    return (
-        <section id="hero" className="h-screen flex items-center justify-left ml-44">
-            <div className="">
-                <h1 className="font-bold uppercase">Kevin van Bommel</h1>
-                <p className="text-7xl font-medium uppercase">Web Developer</p>
-                <p className="mt-16">Applicatieontwikkelaar. Nieuwsgierig, zowel zelfstandig als een teamplayer.</p>
-            </div>
-            <img src={"/assets/foto-kevin.png"} alt="Hero Image" className="ml-32" />
-        </section>
-    )
-}
+import Image from "next/image";
+
+export const Hero = () => {
+  return (
+    <div className="relative w-full flex flex-col-reverse items-center justify-center min-h-[80vh] px-4 py-12 lg:flex-row lg:justify-start lg:items-center lg:px-0 overflow-hidden">
+      <div className="flex flex-col justify-center items-center lg:items-start text-center lg:text-left p-4 rounded-xl lg:ml-10 ">
+        <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-8xl text-white drop-shadow">
+          KEVIN VAN BOMMEL
+        </h1>
+        <p className="text-xl sm:text-2xl md:text-4xl lg:text-6xl font-medium mt-2 ">
+          <span className="text-green-400">WEB</span> <span className="text-blue-800">DEVELOPER</span>
+        </p>
+        <p className="mt-6 sm:mt-8 lg:mt-12 font-light text-base sm:text-lg md:text-xl lg:text-2xl max-w-md text-white">
+          Software Developer. Nieuwsgierig, zowel zelfstandig als een
+          teamplayer.
+        </p>
+      </div>
+      <div className="flex justify-center items-center mb-8 lg:mb-0 lg:ml-32">
+        <Image
+          src="/assets/foto-kevin.png"
+          alt="Hero Image"
+          className="rounded-full shadow-xl border-4"
+          width={300}
+          height={300}
+          priority
+        />
+      </div>
+    </div>
+  );
+};
