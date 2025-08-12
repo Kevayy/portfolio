c3a0c05eb71f4161a99c0f93434e207aa4efd886
Kevayy <kevin@kevay.nl>
2025-08-12 11:42:51 +0200

    Make Hero responsive

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export const Hero = () => {
   return (
-    <div className="relative w-full flex flex-col-reverse items-center justify-center min-h-[80vh] px-4 py-12 lg:flex-row lg:justify-start lg:items-center lg:px-0 overflow-hidden">
+    <div className="relative w-full flex flex-col-reverse justify-center min-h-[80vh] px-4 py-12 lg:flex-row lg:justify-start lg:items-center lg:px-0 overflow-hidden">
       <div className="flex flex-col justify-center items-center lg:items-start text-center lg:text-left p-4 rounded-xl lg:ml-10 ">
         <h1 className="font-bold text-3xl sm:text-4xl md:text-5xl lg:text-8xl text-white drop-shadow uppercase">
           Kevin van Bommel
@@ -10,8 +10,7 @@ export const Hero = () => {
         <p className="text-xl sm:text-2xl md:text-4xl lg:text-6xl font-medium mt-2 uppercase">
           <span className="text-green">Web</span> <span className="text-blue">Developer</span>
         </p>
-        <div className="w-[564px] h-2.5 mt-8 bg-linear-90 from-violet via-blue to-green" />
-        <p className="mt-6 sm:mt-8 lg:mt-12 font-light text-base sm:text-lg md:text-xl lg:text-2xl text-white">
+        <div className="mt-8 h-2.5 bg-linear-90 from-violet via-blue to-green self-center lg:self-start shrink-0 grow-0 basis-auto w-96 sm:w-64 md:w-80 lg:w-[564px] max-w-full" />        <p className="mt-6 lg:mt-10 font-light text-white leading-relaxed text-base sm:text-lg md:text-xl lg:text-2xl break-words">
           Applicatieontwikkelaar. Nieuwsgierig, zowel zelfstandig als een teamplayer.
         </p>
       </div>
